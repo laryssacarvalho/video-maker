df012ec868ab1127d59ac445372d9d7aeac1fda3
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2020-04-29 22:27:47 -0300

    feat: render video with ffmpeg and videoshow

--- robots/video.js
+++ robots/video.js
@@ -1,12 +1,23 @@
 const state = require('./state.js');
 const gm = require('gm').subClass({imageMagick: true});
+const videoshow = require('videoshow');
+const path = require('path');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 
 async function robot() {
     const content = state.load();
 
     await convertAllImages(content);
-    await createAllSentenceImages(content);
     await createYoutubeThumbnail();
+    await createFFmpegScript(content);
+    await renderVideoWithFFmpegAndNode(content);
+
+    state.save(content);
 }
 
 async function convertAllImages(content) {
@@ -53,65 +64,6 @@ async function convertImage(sentenceIndex) {
     });
 }
 
-async function createAllSentenceImages(content) {
-    for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-        await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text);
-    }
-}
-
-async function createSentenceImage(sentenceIndex, sentenceText) {
-    return new Promise((resolve, reject) => {
-        const outputFile = `./content/${sentenceIndex}-sentence.png`;
-
-        const templateSettings = {
-            0: {
-                size: '1920x400',
-                gravity: 'center'
-            },
-            1: {
-                size: '1920x1080',
-                gravity: 'center'
-            },
-            2: {
-                size: '800x1080',
-                gravity: 'west'
-            },
-            3: {
-                size: '1920x400',
-                gravity: 'center'
-            },
-            4: {
-                size: '1920x1080',
-                gravity: 'center'
-            },
-            5: {
-                size: '800x1080',
-                gravity: 'west'
-            },
-            6: {
-                size: '1920x400',
-                gravity: 'center'
-            }
-        }
-    
-        gm()
-            .out('-size', templateSettings[sentenceIndex].size)
-            .out('-gravity', templateSettings[sentenceIndex].gravity)
-            .out('-background', 'transparent')
-            .out('-fill', 'white')
-            .out('-kerning', '-1')
-            .out(`caption:${sentenceText}`)
-            .write(outputFile, (error) => {
-                if (error) {
-                    return reject(error)
-                }
-
-                console.log(`> Sentence created: ${outputFile}`);
-                resolve();
-            });
-    });
-}
-
 async function createYoutubeThumbnail() {
     return new Promise((resolve, reject) => {
         gm()
@@ -124,5 +76,77 @@ async function createYoutubeThumbnail() {
                 resolve();
             });
     })
+}
+
+async function createFFmpegScript(content) {
+    await state.saveScript(content);
+}
+
+async function renderVideoWithFFmpegAndNode(content) {
+    return new Promise((resolve, reject) => {
+        const images = buildImagesArray(content);
+        
+        const audio = path.join(__dirname, '../templates/1/newsroom.mp3');
+        const video = path.join(__dirname, '../content/video-maker.mp4');
+
+        const audioParams = { fade: true, delay: 1 };
+
+        const videoOptions = {
+            fps: 30,
+            loop: 5, // seconds
+            transition: true,
+            transitionDuration: 1, // seconds
+            videoBitrate: 1024,
+            videoCodec: 'libx264',
+            size: '640x?',
+            audioBitrate: '128k',
+            audioChannels: 2,
+            format: 'mp4',
+            pixelFormat: 'yuv420p',
+            useSubRipSubtitles: false,
+            subtitleStyle: {
+                Fontname: 'Verdana',
+                Fontsize: '60',
+                PrimaryColour: '11861244',
+                SecondaryColour: '11861244',
+                TertiaryColour: '11861244',
+                BackColour: '-2147483640',
+                Bold: '2',
+                Italic: '0',
+                BorderStyle: '2',
+                Outline: '2',
+                Shadow: '3',
+                Alignment: '1',
+                MarginL: '40',
+                MarginR: '60',
+                MarginV: '40'
+            }
+        };
+
+        videoshow(images, videoOptions)
+            .audio(audio, audioParams)
+            .save(video)
+            .on('error', function(err, stdout, stderr) {
+                console.error('Error: ', err);
+                console.error('ffmpeg stderr: ', stderr);
+                reject(error);
+            })
+            .on('end', function(output) {
+                resolve();
+            });
+    });
+}
+
+function buildImagesArray(content) {
+    let images = [];
+
+    for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+        images.push({
+            path: `./content/${sentenceIndex}-converted.png`,
+            caption: content.sentences[sentenceIndex].text
+        });
+    }
+
+    return images;
 }
 module.exports = robot;
